a773bc5d31a82006fabd0bcb58296e034918b46a
Amritpal Singh <707763@student.tor.loc>
2019-07-10 08:53:17 -0400

    Assignment 3

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0707763
+{
+   {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tp = new TextProcessing();
+            tp.Run();
+        }
+    }
+
+    class TextProcessing
+    {
+        public void Run()
+        {
+            // Open the names file and access the data
+            using (StreamReader file = new StreamReader("U:\Users\707763\CSD3354-S3-A3"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+    }
+
+}
